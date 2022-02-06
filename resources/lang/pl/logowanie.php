3650a23846a108553115b82da8ba7de915f28102
HAPPY231 <marszycki056@gmail.com>
2022-02-06 11:31:21 +0100

    Translacje/Dynamiczne parametry

---
+++ resources/lang/pl/logowanie.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'button'=>[
+      'save'=>'Zapisz'
+    ],
+    'product'=>[
+            'edit_title'=>'Edycja produktu :name',
+            'add_form'=>[
+                'title' => 'Dodawanie produktu',
+                'name'=>'Nazwa',
+                'description'=>'Opis',
+                'amount'=>'Ilość',
+                'price'=>'Cena',
+                'graphics'=>'Grafika',
+            ],
+
+    ],
+
+];
